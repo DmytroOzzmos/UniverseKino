1f757bda0b2ed08ce3cff9795cade95afc65392d
Anton Nikolenko <nikolenko@kai24.com>
2020-01-22 22:50:07 +0200

    Auth controller

---
+++ UniverseKino.Data/Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniverseKino.Data.Entities
+{
+    class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ UniverseKino.Data/Entities/ClientProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniverseKino.Data.Entities
+{
+    class ClientProfile
+    {
+        [Key]
+        [ForeignKey("ApplicationUser")]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
