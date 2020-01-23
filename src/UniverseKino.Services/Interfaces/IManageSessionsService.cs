ea4c3f35d82a766f55765452aed5a24d8608011c
DmitryOzzmos <dmitry.ozzmos@gmail.com>
2020-01-23 17:37:14 +0200

    Create ManageSession

--- src/UniverseKino.Services/Interfaces/IManageSessionsService.cs
+++ src/UniverseKino.Services/Interfaces/IManageSessionsService.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using UniverseKino.Data.Entities;
 using UniverseKino.Services.Dto;
 
 namespace UniverseKino.Services.Interfaces
 {
     public interface IManageSessionsService
     {
-        // void Add(SessionDTO )
+        void Add(SessionDTO session);
+        void Update(SessionDTO session);
+        bool Remove(int id);
     }
 }
