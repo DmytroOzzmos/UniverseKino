bfb65cb17c9aa0ea0961916053a9fc8c9f06f9ca
DmitryOzzmos <dmitry.ozzmos@gmail.com>
2020-01-25 20:48:31 +0200

    Fix AdminController (change atribute on HttpDelete)

--- src/UniverseKino.WEB/Controllers/AdminController.cs
+++ src/UniverseKino.WEB/Controllers/AdminController.cs
@@ -19,7 +19,7 @@ namespace UniverseKino.WEB
         }
 
         [HttpDelete("sessions/{id}")]
-        public IActionResult DeleteSessinons(int id)
+        public IActionResult DeleteSessinons([FromRoute]int id)
         {
             return Ok();
         }
@@ -33,7 +33,7 @@ namespace UniverseKino.WEB
 
 
         [HttpDelete("movies/{id}")]
-        public IActionResult DeleteMovies([FromQuery] int id)
+        public IActionResult DeleteMovies([FromRoute] int id)
         {
             return Ok();
         }
