4e637a2a44464a26f09a14f45748dbca70a0daf3
DmitryOzzmos <dmitry.ozzmos@gmail.com>
2020-02-09 22:50:31 +0200

    Added Context factory for migrations

---
+++ src/UniverseKino.Data/DesignDBConfigure/DesignTimeDbContextFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using UniverseKino.Data.EF;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+
+namespace UniverseKino.Data.DesignDBConfigure
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<UniverseKinoContext>
+    {
+        public UniverseKinoContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<UniverseKinoContext>();
+
+            var connectionString = configuration.GetConnectionString("Develop");
+            //var connectionString = configuration.GetConnectionString("Realese");
+
+            builder.UseSqlServer(connectionString);
+
+            return new UniverseKinoContext(builder.Options);
+        }
+    }
+}
